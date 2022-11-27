63da947ab3b63f8e8ce9612e36a79c3211011649
Oriol Boix Anfosso <orboan@gmail.com>
2022-11-27 12:25:54 +0100

    ingress ip fixed

--- entorn-start.sh
+++ entorn-start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd ./kube-local/
+./ingress-set-ip.sh
 ./start-k8s-entorn.sh
 
 exit 0
+
---
+++ kube-local/ingress-set-ip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export IP_FROM_CONFIG_FILE=$(grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' ./config.yaml)
+
+echo "IP_FROM_CONFIG_FILE=$IP_FROM_CONFIG_FILE"
+
+export INGRESS_PUBLIC_IP=$(kubectl --namespace=ingress-nginx get all | grep LoadBalancer | tr -s ' ' | cut  -d' ' -f3)
+
+echo "INGRESS_PUBLIC_IP=$INGRESS_PUBLIC_IP"
+
+if [ ! -z "$IP_FROM_CONFIG_FILE" ]; then
+		sed -i "s/$IP_FROM_CONFIG_FILE/$INGRESS_PUBLIC_IP/g" ./config.yaml
+	else
+			sed -i "s/'INGRESS_PUBLIC_IP': \"\"/'INGRESS_PUBLIC_IP': \"$INGRESS_PUBLIC_IP\"/g" ./config.yaml
+fi
+
+NEW_IP_FROM_CONFIG_FILE=$(grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' ./config.yaml)
+
+echo "NEW_IP_FROM_CONFIG_FILE=$NEW_IP_FROM_CONFIG_FILE"
+
+exit 0
+
+
+
