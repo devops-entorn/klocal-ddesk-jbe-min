05b6214d5fa583d1e545caca1eb03c19c4d87512
Oriol Boix Anfosso <orboan@gmail.com>
2022-11-27 12:33:13 +0100

    formatted

--- kube-local/create-ingress.sh
+++ kube-local/create-ingress.sh
@@ -3,4 +3,3 @@ helm upgrade --install ingress-nginx ingress-nginx \
 	  --repo https://kubernetes.github.io/ingress-nginx \
 	    --namespace ingress-nginx --create-namespace
 
-exit 0
--- kube-local/ingress-set-ip.sh
+++ kube-local/ingress-set-ip.sh
@@ -9,9 +9,9 @@ export INGRESS_PUBLIC_IP=$(kubectl --namespace=ingress-nginx get all | grep Load
 echo "INGRESS_PUBLIC_IP=$INGRESS_PUBLIC_IP"
 
 if [ ! -z "$IP_FROM_CONFIG_FILE" ]; then
-		sed -i "s/$IP_FROM_CONFIG_FILE/$INGRESS_PUBLIC_IP/g" ./config.yaml
-	else
-			sed -i "s/'INGRESS_PUBLIC_IP': \"\"/'INGRESS_PUBLIC_IP': \"$INGRESS_PUBLIC_IP\"/g" ./config.yaml
+	sed -i "s/$IP_FROM_CONFIG_FILE/$INGRESS_PUBLIC_IP/g" ./config.yaml
+else
+	sed -i "s/'INGRESS_PUBLIC_IP': \"\"/'INGRESS_PUBLIC_IP': \"$INGRESS_PUBLIC_IP\"/g" ./config.yaml
 fi
 
 NEW_IP_FROM_CONFIG_FILE=$(grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' ./config.yaml)
